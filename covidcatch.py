254acc49ce221aa36a0c69c398851db0ffe00151
gsbaker <george@gsbaker.uk>
2020-05-30 14:48:58 +0100

    Initial setup.

--- covidcatch.py
+++ covidcatch.py
@@ -1,4 +1,13 @@
+import os, sys # allows for creation of platform independent file paths
 import pygame
 from pygame.locals import *
+
+if not pygame.font:
+    print("Warning: fonts disabled")
+
+if not pygame.mixer:
+    print("Warning: sounds disabled")
+
+
 
 pygame.init()
